a3a0fd52ec1bd73cf65ec72d22bd1688bc92a139
vishnuoruphones <vswaroop04@gmail.com>
2023-11-12 04:06:39 +0530

    migrated to prisma

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -9,18 +9,6 @@ export default async function main() {
     });
     console.log('Connected to MongoDB Successfully');
   } catch (error) {
-    console.log('Initial connection failed, retrying in 10 seconds...', error);
-
-    // Wait for 10 seconds and then try to reconnect
-    setTimeout(async () => {
-      try {
-        await mongoose.connect(process.env.MONGO, {
-          useNewUrlParser: true,
-        });
-        console.log('Connected to MongoDB Successfully on second attempt');
-      } catch (secondError) {
-        console.log('Failed to connect on second attempt:', secondError);
-      }
-    }, 10000);
+  
   }
 }
