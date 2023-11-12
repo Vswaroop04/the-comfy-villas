e0d8faf5ca28db6458a818be9071a47e07e40439
vishnuoruphones <vswaroop04@gmail.com>
2023-11-13 04:32:12 +0530

    added prisma client and operation sf

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+// dotenv.config();
 
-export default async function main() {
-  try {
-    await mongoose.connect(process.env.MONGO, {
-      useNewUrlParser: true,
-    });
-    console.log('Connected to MongoDB Successfully');
-  } catch (error) {
+// export default async function main() {
+//   try {
+//     await mongoose.connect(process.env.MONGO, {
+//       useNewUrlParser: true,
+//     });
+//     console.log('Connected to MongoDB Successfully');
+//   } catch (error) {
   
-  }
-}
+//   }
+// }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
-import connectToDb from '@/database/connection';
 import app from '@/setup';
 import session from '@/utils/sessionStore';
 import router from '@/routes/router';
 import errorHandler from './middleware/errorHandler';
-
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 const port = process.env.PORT || 5000;
 
 app.use(session);
@@ -11,7 +11,10 @@ app.use(router);
 app.use(errorHandler);
 
 async function main() {
-	await connectToDb();
+	const user = await prisma.user.create({
+		data: { name: 'Vishnu', phone: 1245, email: 'vswaroop04f@gmail.com', type : 'user', password : 'vishnu2003' },
+	});
+	console.log(user);
 	app.listen(port, async () => {
 		console.log(`The app listening on port ${port}`);
 	});
